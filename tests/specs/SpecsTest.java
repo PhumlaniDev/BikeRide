cb39ca77e0487112db1abbd681819a9a757cce25
PhumlaniDev <60384943+PhumlaniDev@users.noreply.github.com>
2020-04-30 16:33:22 +0200

    intellij commit 30 apr 2020

--- tests/specs/SpecsTest.java
+++ tests/specs/SpecsTest.java
@@ -9,24 +9,23 @@ import bicycles.specification.BicycleFromSpec;
 import bicycles.specification.BicycleSpecification;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SpecsTest {
 
     @Test
     public void bicycleSpecification(){
 
         BicycleSpecification roadBikeSpec = new BicycleSpecification(11,-4, BicycleType.RoadBike);
-        Bicycle roadBike = new BicycleFromSpec(roadBikeSpec);
-        BikeRideOne bikeRideOne = new BikeRideOne();
-        bikeRideOne.ride(roadBike);
+        assertEquals(roadBikeSpec.getAccelerationSpeed(),11);
+        System.out.println(roadBikeSpec.getAccelerationSpeed());
 
         BicycleSpecification mountainBikeSpec = new BicycleSpecification(5,-3, BicycleType.MountainBike);
-        Bicycle mountainsBike = new BicycleFromSpec(mountainBikeSpec);
-        BikeRideOne bikeRideTwo = new BikeRideTwo();
-        bikeRideTwo.ride(mountainsBike);
+        assertEquals(mountainBikeSpec.getAccelerationSpeed(),5);
+        System.out.println(mountainBikeSpec.getAccelerationSpeed());
 
         BicycleSpecification tandemSpec = new BicycleSpecification(12,-7, BicycleType.Tandem);
-        Bicycle tadndem = new BicycleFromSpec(tandemSpec);
-        BikeRideOne bikeRideThree = new BikeRideThree();
-        bikeRideThree.ride(tadndem);
+        assertEquals(tandemSpec.getAccelerationSpeed(),12);
+        System.out.println(tandemSpec.getAccelerationSpeed());
     }
 }
